a855584095d6e07f4d14fdde572d2a4ac81f8ab8
Document <Document@linzhenhendeair.fios-router.home>
2017-12-13 09:44:38 -0500

    github upload

--- src/eclipse_upload_git/uploadGitTest.java
+++ src/eclipse_upload_git/uploadGitTest.java
@@ -1,16 +1,15 @@
 package eclipse_upload_git;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class uploadGitTest {
 		
 		public static void main(String[] args){
 			WebDriver driver;
 			String baseUrl= "https://www.google.com";
-			String CHROME_PATH = "/Users/Document/Downloads/chromedriver.exe";
-			System.setProperty("webdriver.chrome.driver",CHROME_PATH);
-			driver = new SafariDriver();
+			System.setProperty("webdriver.chrome.driver","/Users/Document/Downloads/chromedriver12");
+			driver = new ChromeDriver();
 			driver.get(baseUrl);
 		}
 	
